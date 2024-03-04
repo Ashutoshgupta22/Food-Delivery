dc0a04fec30a6427704caa072fbca6cf1f37c7ba
Ashutosh Gupta <68068357+Ashutoshgupta22@users.noreply.github.com>
2024-03-04 23:57:51 +0530

    implement QuickGrabScreen layout

--- app/src/main/java/com/aspark/spendwiseassign/ui/theme/Color.kt
+++ app/src/main/java/com/aspark/spendwiseassign/ui/theme/Color.kt
@@ -11,3 +11,4 @@ val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
 val AppOrange = Color(0xFFFE724C)
+val gold = Color(0xFFFF9800)
--- app/src/main/java/com/aspark/spendwiseassign/ui/theme/QuickGrabScreen.kt
+++ app/src/main/java/com/aspark/spendwiseassign/ui/theme/QuickGrabScreen.kt
@@ -1,19 +1,27 @@
 package com.aspark.spendwiseassign.ui.theme
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.Star
 import androidx.compose.material.icons.outlined.Search
+import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -24,17 +32,27 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.aspark.spendwiseassign.R
 
 @Composable
 fun QuickGrabScreen(navController: NavController) {
-    QuickGrabTopBar(navController)
 
-    DetailCard()
+    Column {
+        QuickGrabTopBar(navController)
+        Column(
+            Modifier.verticalScroll(rememberScrollState())
+        ) {
+            DetailCard()
+        }
+    }
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -82,12 +100,14 @@ fun QuickGrabTopBar(navController: NavController) {
                     modifier = Modifier.fillMaxWidth(),
                     verticalAlignment = Alignment.CenterVertically
                 ) {
-                        IconButton(
-                            onClick = { navController.popBackStack() }
-                        ) {
-                            Icon(imageVector = Icons.AutoMirrored.Filled.ArrowBack,
-                                contentDescription = "")
-                        }
+                    IconButton(
+                        onClick = { navController.popBackStack() }
+                    ) {
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                            contentDescription = ""
+                        )
+                    }
 
 //                    Spacer(modifier = Modifier.width(135.dp))
 
@@ -111,6 +131,71 @@ fun QuickGrabTopBar(navController: NavController) {
 @Composable
 fun DetailCard() {
 
+    Card(
+        onClick = { /*TODO*/ },
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentHeight()
+            .padding(top = 16.dp, start = 18.dp, end = 18.dp, bottom = 150.dp),
+        shape = RoundedCornerShape(16.dp)
+    ) {
+
+        Column(
+            modifier = Modifier.padding(24.dp)
+        ) {
+
+            Image(
+                painter = painterResource(id = R.drawable.ic_launcher_background),
+                contentDescription = "",
+                contentScale = ContentScale.FillBounds,
+                modifier = Modifier
+                    .clip(RoundedCornerShape(16.dp))
+                    .fillMaxWidth()
+                    .height(300.dp)
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+            Image(
+                painter = painterResource(id = R.drawable.ic_launcher_background),
+                contentDescription = "",
+                contentScale = ContentScale.FillBounds,
+                modifier = Modifier
+                    .clip(RoundedCornerShape(5.dp))
+                    .height(20.dp)
+                    .width(20.dp)
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(text = "Tawa Pulao", fontWeight = FontWeight.Bold,
+                fontSize = 18.sp)
+
+            Spacer(modifier = Modifier.height(10.dp))
+
+            Text(text = "Cooked with basmati rice",
+                fontSize = 18.sp)
+            Text(text = "Rs. 80", fontWeight = FontWeight.Medium,
+                fontSize = 18.sp, modifier = Modifier.padding(vertical = 6.dp))
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(8.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = "4.8", fontWeight = FontWeight.Bold,
+                    fontSize = 18.sp
+                )
+
+                Icon(imageVector = Icons.Default.Star, contentDescription = "",
+                    tint = gold
+               )
+
+                Text(
+                    text = "(65)",
+                    fontSize = 18.sp
+                )
+
+            }
+        }
+
+    }
 
 
 }
